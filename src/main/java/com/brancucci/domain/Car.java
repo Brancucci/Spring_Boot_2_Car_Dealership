28c1b94c7387f7cfe79be4865affec7ef19f4cf6
abrancucci3 <abrancucci3@gatech.edu>
2020-04-12 20:32:23 -0400

    Added unit and integration testing

--- src/main/java/com/brancucci/domain/Car.java
+++ src/main/java/com/brancucci/domain/Car.java
@@ -26,4 +26,13 @@ public class Car {
         this.owner = owner;
     }
     public Car(){}
+
+    public Car(String brand, String model, String color, String registerNumber, int year, int price){
+        this.brand= brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
 }
